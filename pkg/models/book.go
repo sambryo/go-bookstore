37cedfd188d038832bad2e8e13015f03952fd22e
Samuel Terefe <terefesam@gmail.com>
2022-04-23 13:25:58 -0700

    added controller

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -7,17 +7,39 @@ import (
 
 var db *gorm.DB
 
-type (
-	Book struct {
-		gorm.model
-		Name         string `gorm:""json:"name"`
-		Author       string `json:"author"`
-		Publications string `json:"publications"`
-	}
-)
+type Book struct {
+	gorm.Model
+	Name         string `gorm:""json:"name"`
+	Author       string `json:"author"`
+	Publications string `json:"publications"`
+}
 
 func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookByID(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
 }
