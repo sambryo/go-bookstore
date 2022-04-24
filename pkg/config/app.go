a18b20b7d07b9196032da492ca9d7148bd2b87d6
Samuel Terefe <terefesam@gmail.com>
2022-04-24 14:03:58 -0700

    db config added

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,7 +14,20 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "samuel:passw@rd$@/simpletest?charset=utf8&parseTime=True&loc=Local")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env files")
+		return
+	}
+	host := os.Getenv("DB_DEV_HOST")
+	port := os.Getenv("DB_DEV_PORT")
+	dbname := os.Getenv("DB_DEV_NAME")
+	user := os.Getenv("DB_DEV_USER")
+	password := os.Getenv("DB_DEV_PASSWORD")
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	d, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -7,7 +7,7 @@ import (
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/books/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
